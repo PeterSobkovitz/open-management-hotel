edd76a0edd8f761257eb0f98c666850da9668d55
p <dick>
2023-12-25 21:23:20 +0100

    getting all the room test performed, assert works

---
+++ room_routers.js
@@ -0,0 +1,51 @@
+
+const express = require('express');
+const Room = require('./room_model');
+const router = express.Router();
+
+// Get all rooms
+router.get('/rooms', async (req, res) => {
+    try {
+        console.log("called");
+        const rooms = await Room.find({});
+        res.send(rooms);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// Get room by ID
+router.get('/rooms/:id', async (req, res) => {
+    try {
+        const room = await Room.findById(req.params.id);
+        if (!room) {
+            return res.status(404).send();
+        }
+        res.send(room);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+// Filter rooms
+router.get('/rooms/filter', async (req, res) => {
+    const match = {};
+
+    if (req.query.maxOccupancy) {
+        match.maxOccupancy = req.query.maxOccupancy;
+    }
+    if (req.query.priceRange) {
+        const [minPrice, maxPrice] = req.query.priceRange.split(',').map(Number);
+        match.pricePerNight = { $gte: minPrice, $lte: maxPrice };
+    }
+    // Add more filters as needed
+
+    try {
+        const rooms = await Room.find(match);
+        res.send(rooms);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const express=require("express");
 
 const mongoose=require("mongoose");
 const userRouter=require('./routes');
-
+const roomRouter=require('./room_routers');
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -16,6 +16,7 @@ mongoose.connect(process.env.MONGODB_URL, {
 
 app.use(express.json());
 app.use(userRouter);
+app.use(roomRouter);
 app.get('/',(req,res)=>{
     res.send("HOMEPage");
 })
